e840c49782a58603f7ca8ca09d49885020f3e9d3
Przemyslaw Swierczek <swierq@gmail.com>
2023-12-20 17:58:23 +0100

    Adding HELM.

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,6 +14,21 @@ provider "kubernetes" {
   }
 }
 
+provider "helm" {
+  kubernetes {
+    host                   = module.eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      command     = "aws"
+      # This requires the awscli to be installed locally where Terraform is executed
+      args = ["eks", "get-token", "--cluster-name", "k8slab"]
+    }
+
+  }
+}
+
 
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
@@ -75,6 +90,13 @@ module "eks" {
     }
   }
 }
+
+module "alb-ingress-controller" {
+  source       = "campaand/alb-ingress-controller/aws"
+  version      = "2.0.0"
+  cluster_name = module.eks.cluster_name
+}
+
 
 resource "kubernetes_namespace" "k8s" {
   metadata {
--- terraform/versions.tf
+++ terraform/versions.tf
@@ -8,6 +8,10 @@ terraform {
       source  = "hashicorp/kubernetes"
       version = "~> 2.24.0"
     }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.12.0"
+    }
   }
   required_version = "~> 1.5.7"
 }
